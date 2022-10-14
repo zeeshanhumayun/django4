59f645a049ba478ab0062baa5a5a7b6cca55bb8a
zeeshan <zeeshantalking@gmail.com>
2022-10-14 18:05:45 +0500

    port updated

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,9 +3,9 @@
 
 echo "Initializing postgres db..."
 
-while ! nc -z $DB_HOST $DB_PORT; do
-  sleep 1
-done
+#while ! nc -z $DB_HOST $DB_PORT; do
+ # sleep 1
+#done
 
 echo "postgres database has initialized successfully"
 fi
